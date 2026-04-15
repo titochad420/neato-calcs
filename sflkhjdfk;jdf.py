e4e615ec8ac41b53197d8713964c56daac3895bb
Fragmaster Tito <harveylates@gmail.com>
2026-04-15 17:11:38 +1000

    gfd

--- sflkhjdfk;jdf.py
+++ sflkhjdfk;jdf.py
@@ -18,6 +18,8 @@ top_label.grid(row=0, column=0, padx=10, pady=10)
 entry = tk.Entry(root)
 entry.grid(row=1, column=0, padx=10, pady=10)
 
+mult_label = tk.Label(root, text="Enter the divisor to scale the radius (e.g., 1 (stock ksp), 2.5(Jnsq) 10(),):")
+mult_label.grid(row=0, column=1, padx=10, pady=10)
 entry2 = tk.Entry(root)
 entry2.grid(row=1, column=1, padx=10, pady=10)
 
@@ -25,8 +27,8 @@ def calculate_gravity():
     global Schildradius
     try:
         Schildradiusradius = float(entry.get())
-        divider = float(entry2.get())
-        Scaledradius = (Schildradiusradius / divider)
+        scaler = float(entry2.get())
+        Scaledradius = ((Schildradiusradius / 10) * scaler)
         gravity = (Scaledradius / 5) ** 2
         result_label.config(text=f"Calculated gravity: {gravity:.3f}")
     except ValueError:
